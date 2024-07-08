a357c7becf5accd64e4cfe773f924a763cfb4a2f
Amar Verma <amarverma.lnct@gmail.com>
2024-07-08 09:43:01 +0530

    fixed title

--- app/_components/Title.tsx
+++ app/_components/Title.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = ({ title }: any) => {
 	return (
 		<div>
-			<div className="p-12 border-b-2 flex justify-center  border-blue-500 text-3xl uppercase  font-medium ">
+			<div className="border-b-2 flex justify-center  border-blue-500 text-3xl uppercase  font-medium ">
 				<strong className="">{title}</strong>
 			</div>
 		</div>
