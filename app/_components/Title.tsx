d48367318f22ccd916a4f0194475d27ac0587b7e
Amar Verma <amarverma.lnct@gmail.com>
2024-07-08 09:44:22 +0530

    update

--- app/_components/Title.tsx
+++ app/_components/Title.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = ({ title }: any) => {
 	return (
 		<div>
-			<div className="border-b-2 flex justify-center  border-blue-500 text-3xl uppercase  font-medium ">
+			<div className="border-b-2 pb-4 flex justify-center  border-blue-500 text-2xl uppercase  ">
 				<strong className="">{title}</strong>
 			</div>
 		</div>
